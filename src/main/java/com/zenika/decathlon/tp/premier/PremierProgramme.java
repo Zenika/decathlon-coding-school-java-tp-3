98e8d19146579cc6c828d12c11131774c9f51306
fcorni25 <francoise.cornille@decathlon.com>
2019-03-19 11:21:29 +0100

    Mon premier commit

--- src/main/java/com/zenika/decathlon/tp/premier/PremierProgramme.java
+++ src/main/java/com/zenika/decathlon/tp/premier/PremierProgramme.java
@@ -25,5 +25,13 @@ public class PremierProgramme {
 	
 	public static int fibonacci(int value) {
 		// TODO implémenter cette fonction
+		if (value == 0 || value == 1) {
+			return value;
+			} else
+		{
+			return fibonacci(value-1) +  fibonacci(value -2);
+		}
+		
+		
 	}
 }
